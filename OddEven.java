0a56ae505e6b284271aa333068c923829dddee6b
prashant_nv <singh.p@novelvox.com>
2022-10-02 12:14:51 +0530

    Hello World

---
+++ OddEven.java
@@ -0,0 +1,9 @@
+import java.lang.System.Logger;
+
+//Write a program to print whether a number is even or odd, also take input.
+//print classic hello world
+public class OddEven {
+    public static void main(String[] args) {
+        System.out.println("Hello my world");
+    }
+}
