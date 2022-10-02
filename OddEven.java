bb80f9f229d94e3342675217b0624677494f59ce
“Prashant <psprashantsingh91@gmail.com>
2022-10-02 17:07:09 +0530

    OddEven check

---
+++ OddEven.java
@@ -0,0 +1,16 @@
+// Write a program to print whether a number is even or odd, also take input.
+import java.util.Scanner;
+public class OddEven {
+    public static void main(String[] args) {
+        System.out.println("Input a number: ");
+        Scanner scn = new Scanner(System.in);
+        int inputNumber = scn.nextInt();
+
+        if(inputNumber % 2 == 0){
+            System.out.println("Input number "+inputNumber+ " is EVEN");
+        }
+        else{
+            System.out.println("Input number "+inputNumber+ " is ODD");
+        }
+    }
+}
