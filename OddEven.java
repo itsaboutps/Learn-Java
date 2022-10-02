fafc70a2a2435a4fd2b549e697d4b42d9d7d1419
“Prashant <psprashantsingh91@gmail.com>
2022-10-02 14:24:59 +0530

    oddEve

--- OddEven.java
+++ OddEven.java
@@ -1,6 +1,3 @@
-import java.lang.System.Logger;
-
-//Write a program to print whether a number is even or odd, also take input.
 //print classic hello world
 public class OddEven {
     public static void main(String[] args) {
