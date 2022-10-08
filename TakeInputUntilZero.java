8120f0378e6f48acd285a27ee3ce94b5f9ff64a8
Prashant <psprashantsingh91@gmail.com>
2022-10-08 10:39:09 +0530

    TakeInputUntilUserEntersZero

--- TakeInputUntilZero.java
+++ TakeInputUntilZero.java
@@ -1,1 +1,21 @@
+//Take integer inputs till the user enters 0 and print the sum of all numbers (HINT: while loop)
+import java.util.Scanner;
 
+public class TakeInputUntilZero{
+    public static void main(String[] args) {
+        int sum=0;
+        Scanner sc=new Scanner(System.in);
+        int number= sc.nextInt();
+        while(number>0){
+            sum+=number;
+            number= sc.nextInt();
+
+
+            //apply the debugger here to see the catch here
+            if (number==0){
+                break;
+            }
+        }
+        System.out.println(sum);
+    }
+}
