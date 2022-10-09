4c0b4ce63f238f85cd5a28414eaa43df69980608
Prashant <psprashantsingh91@gmail.com>
2022-10-09 06:19:04 +0530

    taking input and printing output

---
+++ Main.java
@@ -0,0 +1,9 @@
+//taking input and printing its output until the next line comes in
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextLine());
+    }
+}
