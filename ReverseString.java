9107a3ccccaa7a0e99eb36604cd12a2c00d81dd9
Prashant <psprashantsingh91@gmail.com>
2022-10-09 19:18:06 +0530

    Reverse a String

---
+++ ReverseString.java
@@ -0,0 +1,19 @@
+// Reverse A String In Java
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string ");
+        String inp=sc.nextLine();
+        ReverseString rev=new ReverseString();
+        rev.revString(inp);
+    }
+    private void revString(String inps1){
+        char c[]=inps1.toCharArray();
+        for (int i=inps1.length()-1;i>=0;i--) {
+            System.out.print(inps1.charAt(i));
+        }
+
+    }
+}
