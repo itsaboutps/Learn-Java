c4eda70b7b622cf2ffe3e3624088b3636d962cee
Prashant <psprashantsingh91@gmail.com>
2022-10-10 10:20:10 +0530

    Leetcode-1929_ Concatenation of Arrays

---
+++ LeetcodeProblems/ArrayConcatenation.java
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/concatenation-of-array/
+//1929. Concatenation of Array
+package LeetcodeProblems;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ArrayConcatenation {
+    public static void main(String[] args) {
+        int []arr={1,2,1};
+        concatArr(arr);
+    }
+    public static void concatArr(int[] nums){
+        int[] result = new int[nums.length * 2];
+	    for(int i=0;i<nums.length;i++)
+		result[i + nums.length] = result[i] = nums[i];
+        System.out.println(Arrays.toString(result));
+    }
+}
